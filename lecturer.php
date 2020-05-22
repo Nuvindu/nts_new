5030c14e4674b10af1538d4bd82b4d04582efd6e
vinoja <63710147+vinoja98@users.noreply.github.com>
2020-05-22 21:26:24 +0530

    Update lecturer.php

--- lecturer.php
+++ lecturer.php
@@ -65,6 +65,7 @@
                 <li><a href="student-profile.php"><i class="fas fa-user"></i>Profile</a></li>
                 <li><a href="#"><i class="fas fa-address-card"></i>About</a></li>
                 <li><a href="go-to-results.php"><i class="fas fa-project-diagram"></i>Results</a></li>
+		<li><a href="go-to-exam-timeTables.php"><i class="fas fa-project-diagram"></i>Exam Time Tables</a></li>
                 <li><a href="#"><i class="fas fa-blog"></i>Blogs</a></li>
                 <li><a href="#"><i class="fas fa-address-book"></i>Hostel Info</a></li>
             </ul>
