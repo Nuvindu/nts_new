15fdad21ce5497d7b0f4ba8ba67f38f80757d50e
vinoja <63710147+vinoja98@users.noreply.github.com>
2020-07-19 11:10:34 +0530

    Update login -responsive

--- login.php
+++ login.php
@@ -4,29 +4,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="css/log.css">
+	<title>NTS Login Form</title>
+	<link rel="stylesheet" type="text/css" href="css/style-login.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body background="img/green.jpg">
-	<div class="icon"><img src="img/home.ico" width="22" height="22"></div>
-	<div class="top"><a href="index.php">Home</a></div>
-	<div class="mid clearfix">
-		<h3>&nbsp;&nbsp;Login &nbsp;&nbsp;</h3>
-		<div class="mid-right clearfix">
+<body>
+<div class="icon" style="float:right;padding-right:1%;"><img src="img/home.ico" width="22" height="22"></div>
+<div class="top" style="float:right;font-size:3rem"><a href="index.php">Home</a></div>
+
+	<img class="wave" src="img/wave.png">
+	<div class="container">
+		<div class="img">
+			<img src="img/n4.jpg">
+		</div>
+		<div class="login-content">
 			<form action="login.php" method="post">
 			<?php 
 					if (!empty($errors)) {
 						display_errors($errors);
 					}
 			?>	
-			<input type="text" name="index_no" id="" placeholder="Index Number">
-			<br/>
-			<br/>
-			<input type="password" name="password" id="" placeholder="Password"><br/><br/>
-			<button type="submit" name="submit">Log In</button>
-		</div>
-	</div>
+			<h2 style="font-size:1.5rem;">Nurses Training School<br>Galle</h2>
+			
+				<img src="img/logo-0.png">
+				<h2 style="font-size:1.5rem;">Welcome</h2>
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Username</h5>
+           		   		<input type="text"  name="index_no" id="" class="input">
+           		   </div>
+           		</div>
+           		<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Password</h5>
+           		    	<input type="password"  name="password" id="" class="input">
+            	   </div>
+            	</div>
+            	<a href="#">Forgot Password?</a>
+            	<input type="submit" class="btn" name="submit" value="Login">
+            <!-- </form> -->
+        </div>
+    </div>
+    <script type="text/javascript" src="js/login.js"></script>
 </body>
 </html>
-
-<?php mysqli_close($connection); ?>
