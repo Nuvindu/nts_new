1a75598a959452abf7f821ffd78f2a388ad39c42
vinoja <63710147+vinoja98@users.noreply.github.com>
2020-07-22 19:04:12 +0530

    Update modify_year.php

--- modify_year.php
+++ modify_year.php
@@ -1,5 +1,7 @@
 <html>  
-    <head>  
+    <head>
+        <meta charset="UTF-8"> 
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Update Student Year</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
