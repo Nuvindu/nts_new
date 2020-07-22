000845976b18b8757fa790debb93552e0a28ebf5
vinoja <63710147+vinoja98@users.noreply.github.com>
2020-07-22 19:05:29 +0530

    Update delete_students.php

--- delete_students.php
+++ delete_students.php
@@ -8,6 +8,8 @@ $result = mysqli_query($connection, $query);
 <!DOCTYPE html>
 <html>
  <head>
+  <meta charset="UTF-8"> 
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Delete Students</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
