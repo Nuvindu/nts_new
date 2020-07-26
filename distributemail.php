6788156537145596160f566b6d69fec4aeaaac2f
vinoja <63710147+vinoja98@users.noreply.github.com>
2020-07-26 19:05:36 +0530

    Responsive distributed mail

--- distributemail.php
+++ distributemail.php
@@ -45,32 +45,41 @@ if(isset($_POST['submit'])) {
 <html>
 <head>
 	<title>Send Notifications</title>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/distributeemail.css">
-	<link href='https://fonts.googleapis.com/css?family=Cabin Sketch' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="css/feedback.css">
+	<link rel="stylesheet" href="./style/style-header.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
 </head>
 <body>
-	<h2>Send Notifications</h2>
-	<form action="distributemail.php" method="post" class="userform">
-        <label for="">Send To:</label>
-        <select name="receiver" id="receiver"style="border: black;border-radius: 3px;padding: 5px;padding-right: 20px;">
-        	<option value = 'Lecturer' selected> Lecturers </option>
-        	<option value = 'Student'> Students </option>
-        </select>
-		<br><br>
-		<label for="">Subject</label>
-		<input type="text" class = "subject" name="subject">
-		<br><br>
-		<label for="" class="messagelabel">Message</label>
-		<br>
-		<textarea type="text" class = "message" name="message"></textarea>
-		<br>
-		<button name="submit">Submit</button>
-	</form>
-
-
-
-
-</body>
+<header>
+	<div class="back" style="float:right;"><a href="operator.php">&gt&gtBack</a></div>
+    <div class="icon"><img src="img/home.ico" width="22" height="22"><a href="index.php">Home</a></div>
+        
+        
+    </header>
+   
+	<!-- header -->
+    <div class="header">
+            <?php include_once('header.php'); ?>
+    </div>
+     
+     <div class="container" style="padding-bottom:10%;">
+		<div class="contact-box">
+			<div class="left"></div>
+			<div class="right">
+				<h2>Send Notifications</h2>
+				<form action="distributemail.php" method="post" class="userform">
+				<select name="receiver" id="receiver" input type="text" class="field" placeholder="Receiver" >
+					<option value = 'Lecturer'> Lecturers </option>
+     				<option value = 'Student'> Students </option>
+        		</select>
+				<input type="text" class="field" placeholder="Subject" name="subject">
+				<textarea placeholder="Feedback" class="field" name="message"></textarea>
+				<button name="submit" class="btn">Send</button>
+			</form>
+			</div>
+			
+		</div>
+	</div>
+	</body>
 </html>
